6e10615f9cb6c821bef13bbaddcb4423979fd438
lionelritchie29 <lionelrtchieee@gmail.com>
2022-08-05 14:02:39 +0700

    feat: show user  connect/disconnect log in application

--- client/main.js
+++ client/main.js
@@ -1,2 +1,19 @@
 const socket = io();
 console.log('test');
+
+socket.on('connect', () => {
+  console.log('connected');
+});
+
+socket.on('logs', (logData) => {
+  const logList = document.getElementById('logs');
+  const logItem = document.createElement('li');
+  logItem.classList.add('text-xs', 'border-b', 'mb-2', 'pb-2');
+  logItem.innerHTML += `<p class="text-blue-600">[${logData.date}]</p>`;
+  logItem.innerHTML += `<p class="text-gray-600">${logData.message}</p>`;
+  logList.appendChild(logItem);
+});
+
+socket.onAny((eventName, ...args) => {
+  console.log(eventName);
+});
--- server/chat-server.js
+++ server/chat-server.js
@@ -1,3 +1,4 @@
+const { format } = require('date-fns');
 const { Server } = require('socket.io');
 
 class ChatServer {
@@ -9,12 +10,23 @@ class ChatServer {
 
   start() {
     this.io.on('connection', (socket) => {
-      console.log(`new user connected: ${socket.id}`);
+      console.log(`new user: ${socket.id} connected`);
+      this.io.emit('logs', this.createLogMessage(`A new user (${socket.id}) has connected`));
 
       socket.on('disconnect', () => {
         console.log(`user ${socket.id} disconnected`);
+        this.io.emit('logs', this.createLogMessage(`A user (${socket.id}) has disconnected`));
       });
     });
+  }
+
+  createLogMessage(message) {
+    const currDate = new Date();
+    const formattedDate = format(currDate, 'dd-MM-yyyy kk:mm:ss');
+    return {
+      date: formattedDate,
+      message,
+    };
   }
 }
 
